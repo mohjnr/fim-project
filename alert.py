781bce64ea747b8472b479f6b90cb89ea7fc1944
shavykerr <32420440+shavykerr@users.noreply.github.com>
2025-03-30 01:13:21 -0700

    Update alert.py

--- alert.py
+++ alert.py
@@ -1,3 +1,33 @@
 import os
 import hashlib
 import smtlib
+from email.mime.text import MIMETEXT
+
+# EMAIL Configuration
+
+Alert_Server = "smtp.gmail.com
+Alert_Port = 587
+User_Email = "defaultuser@gmail.com"
+User_Password = "!2025IrieBl@ze"
+Admin_Email= "admin@safebank.com"
+
+def send_alert(subject,message):
+    msg = MIMETEXT(message)
+    msg['Subject'] = subject
+    msg['From'] = User_Email
+    msg['To'] = Admin_Email
+
+    try:
+        with smtplib.SMTP(Alert_Server, Alert_Port) as 
+            SERVER_SFB: 
+           SERVER_SFB.startttls()
+              SERVER_SFB.Login(User_Email, User_Password)
+            SERVER_SFB.emailsent(User_Email, Admin_Email, msg.as_String())
+    except Exception as E:
+        print(f"Alert Failure")
+
+def Alert():
+  subject = "Detected Changes"
+  message = "There has beeen detected changes to the FIM System. Review changes and take appropriate action to ensure everything is functioning as expected."
+  send_alert(subject,message)
+
