5d5d98c865243610dcf08af82bb334ff1b580f08
shavykerr <32420440+shavykerr@users.noreply.github.com>
2025-03-31 09:17:15 -0500

    Comments alert.py

--- alert.py
+++ alert.py
@@ -4,6 +4,7 @@ import smtlib
 from email.mime.text import MIMETEXT
 
 # EMAIL Configuration
+# Credentials for alert notification
 
 Alert_Server = "smtp.gmail.com
 Alert_Port = 587
@@ -11,12 +12,16 @@ User_Email = "defaultuser@gmail.com"
 User_Password = "!2025IrieBl@ze"
 Admin_Email= "admin@safebank.com"
 
+# Function for sending alerts
+
+# Defining alert message information 
 def send_alert(subject,message):
     msg = MIMETEXT(message)
     msg['Subject'] = subject
     msg['From'] = User_Email
     msg['To'] = Admin_Email
-
+    
+# Sending an email alert using the Simple Mail Transfer Protocol (SMTP)
     try:
         with smtplib.SMTP(Alert_Server, Alert_Port) as 
             SERVER_SFB: 
@@ -25,7 +30,8 @@ def send_alert(subject,message):
             SERVER_SFB.sendmail(User_Email, Admin_Email, msg.as_String())
     except Exception as E:
         print(f"Alert Failure")
-
+        
+# Alert function
 def Alert():
   subject = "Detected Changes"
   message = "There has beeen detected changes to the FIM System. Review changes and take appropriate action to ensure everything is functioning as expected."
